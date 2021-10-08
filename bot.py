cef184b55eda4f1b9088079db5dda24f151e7fef
revanhuseynov12 <50525468+revanhuseynov12@users.noreply.github.com>
2021-10-09 01:18:55 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def start(event):
   await event.reply("**LoungeTagger Bot**, Grup veya kanaldaki neredeyse tüm üyelerden bahsedebilirim ★\nDaha fazla bilgi için **/help**'i tıklayın.",
                     buttons=(
                       [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/loungetaggerbot?startgroup=a'),
-                      Button.url('📣 Support', 'https://t.me/loungesupport'),
+                      Button.url('📣 Support', 'https://t.me/midoffnews'),
                       Button.url('🚀 Sahibim', 'https://t.me/hrevan16')]
                     ),
                     link_preview=False
@@ -39,7 +39,7 @@ async def help(event):
   await event.reply(helptext,
                     buttons=(
                       [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/loungetaggerbot?startgroup=a'),
-                       Button.url('📣 Support', 'https://t.me/loungesupport'),
+                       Button.url('📣 Support', 'https://t.me/midoffnews'),
                       Button.url('🚀 Sahibim', 'https://t.me/hrevan16')]
                     ),
                     link_preview=False
@@ -106,5 +106,5 @@ async def mentionall(event):
         usrtxt = ""
 
 
-print(">> Bot çalıyor merak etme 🚀 @loungesupport bilgi alabilirsin <<")
+print(">> Bot çalıyor merak etme 🚀 @midoffnews bilgi alabilirsin <<")
 client.run_until_disconnected()
